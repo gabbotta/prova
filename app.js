f998ffa70f6d409e86a0390b2d2ddb91a3e36b2b
gabriele <gabriele.bottamedi@studenti.unitn.it>
2019-01-16 09:02:45 +0100

    package unico

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const app = require('./app');
+const http = require('http');
 const express = require('express');
 const app = express();
 const quadrato = require('./quadrato');
@@ -7,8 +9,20 @@ app.get('/:input', (req, res) => {
     const x = req.params.input
     res.status(200).json({
     message: 'il quadrato della lunghezza è:'+
-    quadrato.q(x)
+    static q(x){
+        if ((typeof x) === "string"){
+            return Math.pow(x.length,2)
+        }
+        else {
+            return -1
+        }
         })  
 });
 
+
+const port = process.env.PORT || 3000
+
+const server = http.createServer(app);
+
+server.listen(port);
 module.exports = app;
--- server.js
+++
@@ -1,9 +0,0 @@
-//node const http = require('http');
-const app = require('./app');
-const http = require('http');
-
-const port = process.env.PORT || 3000
-
-const server = http.createServer(app);
-
-server.listen(port);
